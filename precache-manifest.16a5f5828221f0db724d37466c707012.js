5332c6f28de1d4ffac99473d10cc21f29de43bb5
unknown <balaban.diana@gmail.com>
2020-02-19 09:34:26 +0100

    Updates

---
+++ precache-manifest.16a5f5828221f0db724d37466c707012.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a747bdce337fe207b52cd73f9ed151e",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "c98592456a9d4e30c1fc",
+    "url": "/weather/static/css/main.be38a56f.chunk.css"
+  },
+  {
+    "revision": "8cc2d3c2d1e53ee02bc8",
+    "url": "/weather/static/js/2.c77515c6.chunk.js"
+  },
+  {
+    "revision": "e0ad3387081147deea4b08b0e0fff77f",
+    "url": "/weather/static/js/2.c77515c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c98592456a9d4e30c1fc",
+    "url": "/weather/static/js/main.1336fbc2.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  },
+  {
+    "revision": "deae9362db55543b1570411d0e9a181c",
+    "url": "/weather/static/media/ParisFrance_Banner copy.deae9362.jpg"
+  },
+  {
+    "revision": "b9249d55905c1a53d7ba1ebd57a3b2e1",
+    "url": "/weather/static/media/amsti.b9249d55.jpg"
+  },
+  {
+    "revision": "b1107a9a2179525114ee5d3239aad91a",
+    "url": "/weather/static/media/background-amsterdam3.b1107a9a.jpg"
+  },
+  {
+    "revision": "e08cd9b32f20b23d671d425a5a9e8a8a",
+    "url": "/weather/static/media/london-banner.e08cd9b3.jpg"
+  },
+  {
+    "revision": "861783885aa6333507062c0af9598a23",
+    "url": "/weather/static/media/london3.86178388.jpg"
+  },
+  {
+    "revision": "63893146cfd5e689720fc1f58e801dac",
+    "url": "/weather/static/media/paris2.63893146.jpg"
+  }
+]);
